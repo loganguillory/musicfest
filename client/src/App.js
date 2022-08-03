14a6d6592dab9f15168ee895f420c219ae813c60
Sabiha Bary <sabihaab9@gmail.com>
2022-08-03 11:38:17 -0400

    added components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
-
+import React from "react";
 import './App.css';
+import ConcertLineUp from './Components/ConcertLineUp';
+import HomePage from './Components/HomePage';
+import ShoppingCart from './Components/ShoppingCart';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>hello world</h1>
+      <ConcertLineUp />
+      <HomePage />
+      <ShoppingCart />
     </div>
   );
 }
---
+++ client/src/components/Tickets.js
@@ -0,0 +1,28 @@
+import React from "react";
+import '../App.css'
+
+
+
+function Tickets() {
+    return (
+        <div className="homepage">
+            <nav className="nav">
+                <ul>
+                    <li>
+                        <a href="Home">Home</a>
+                    </li>
+                    <li>
+                        <a href="Tickets">Tickets</a>
+                    </li>
+                    <li>
+                        <a href="Lineup">Lineup</a>
+                    </li>
+                </ul>
+                    <a className="request" href="AddRequest">Add Request for Item</a>
+            </nav>
+        </div>
+    )
+  }
+  
+export default Tickets;
+
