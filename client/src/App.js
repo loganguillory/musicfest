7b704c1e6009995377a0e57f6484192c68fe5d30
Logan Guillory <97050508+loganguillory@users.noreply.github.com>
2022-08-04 14:30:10 -0400

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,66 +1,5 @@
-import React from 'react';
 //import {Route, Switch} from "react-router-dom"
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom"
-
-
-<<<<<<< HEAD
-import './App.css';
-
-import NavBar from './components/NavBar';
-import ConcertLineUp from './components/ConcertLineUp';
-import HomePage from './components/HomePage';
-import Reviews from './components/Reviews';
-import ShoppingCart from './components/ShoppingCart';
-import SignUp from './components/SignUp';
-
-
-function App() {
-  
-  return (
-    <Router>
-      <NavBar />
-      <Switch>   
-      <Route path="/ConcertLineUp">
-          <ConcertLineUp />
-        </Route>    
-        <Route path="/HomePage">
-          <HomePage />
-        </Route>
-        <Route path="/Reviews">
-          <Reviews />
-        </Route>
-        <Route path="/ShoppingCart">
-          <ShoppingCart />
-        </Route>
-        <Route path="/SignUp">
-          <SignUp />
-        </Route>
-      </Switch>
-    </Router>
-  );
-}
-//   return (
-  
-//       <Switch>   
-//       <Route path="/ConcertLineUp">
-//           <ConcertLineUp />
-//         </Route>    
-//         <Route path="/HomePage">
-//           <HomePage />
-//         </Route>
-//         {/* <Route path="/ProductPage">
-//           <ProductPage />
-//         </Route> */}
-//         {/* <Route path="/">
-//           <Home />
-//         </Route> */}
-//       </Switch>
-
-//   );
-// }
-
-export default App;
-=======
 import React,{ useState, useEffect } from "react"
 import './App.css'
 import ConcertLineUp from './components/ConcertLineUp'
@@ -68,7 +7,7 @@ import HomePage from './components/HomePage'
 import ShoppingCart from './components/ShoppingCart'
 import NavBar from './components/NavBar'
 import Review from './components/Review'
-import {Switch, BrowserRouter as Router, Route, NavLink} from "react-router-dom"
+
 
 function App() {
   const [user, setUser] = useState({});
@@ -135,4 +74,3 @@ function App() {
 }
 
 export default App;
->>>>>>> 15cf15fa88b98620c6cead63f356156a5b8ea264
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,34 +1,5 @@
 import React from "react";
 import '../App.css'
-<<<<<<< HEAD
-
-//need to add a title and dates of musicfest on navbar
-function NavBar() {
-    return (
-        <div className="homepage">
-        <nav className="nav bar">
-            <ul>
-                <li>
-                    <a href="Home">Home</a>
-                </li>
-                <li>
-                    <a href="Cart">Cart</a>
-                </li>
-                <li>
-                    <a href="Reviews">Reviews</a>
-                </li>
-                <li>
-                    <a href="Lineup">Lineup</a>
-                </li>
-                <li>
-                    <a href="SignUp">Sign Up</a>
-                </li>
-            </ul>
-        </nav>
-    </div>
-    );
-}
-=======
 import {NavLink} from "react-router-dom"
 
 
@@ -63,7 +34,6 @@ function NavBar() {
         </div>
     )
   }
->>>>>>> 15cf15fa88b98620c6cead63f356156a5b8ea264
   
 export default NavBar;
 
